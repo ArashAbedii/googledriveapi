e61ee460b9e175f9187a8da5e15a62ad04110e79
Arash <arashabedi998@gmail.com>
2021-09-12 10:20:08 +0430

    fixed stuff

--- index.php
+++ index.php
@@ -1,14 +1,9 @@
 <?php
 include 'Drive.php';
 
-$clientId='1012604247986-7mmsi3hf2kojed8s21mapfst7afhj21g.apps.googleusercontent.com';
-$clientSecret='0S9jUf6i3ahuHz5rbO3dttTT';
-$code="4/2F0AX4XfWgNcsFsaIkobGmU0zDzJkhL0h1ZZjh-1cNk4YH3Mb6FTDxLfkdQzGqZta2YFtQG1Q";
-$redirectUri="http://localhost:8000";
-
-//Drive::getCode($clientId,$redirectUri,'https://www.googleapis.com/auth/drive.file','code',['access_type'=>'offline']);
-//echo Drive::getAccessToken($clientId,$clientSecret,$code,$redirectUri);
-//echo Drive::refreshToken($clientId,$clientSecret,'1//09n_atf3lcJ0OCgYIARAAGAkSNwF-L9IrKm1doP23yN444UZO6W2ifZ-TWbmseGiZgOZzJUaiE8DKbBTvD-C2DA_8xTp6zBBz3Vg');
+//Drive::getCode(CLIENT_ID,REDIRECT_URI,'https://www.googleapis.com/auth/drive.file','code',['access_type'=>'offline']);
+//echo Drive::getAccessToken(CLIENT_ID,CLIENT_SECRET,file_get_contents('code.txt'),REDIRECT_URI);
+//echo Drive::refreshToken(CLIENT_ID,CLIENT_SECRET,REFRESH_TOKEN);
 //echo Drive::getAccessTokenSimply();
 //Drive::simpleUpload('https://irsv.upmusics.com/99/Armin%20Vigen%20%7C%20Pashimoon%20(128).mp3?_ga=2.29221692.148825670.1631424076-2052873322.1631424076');
 //Drive::downloadFile('1oa82caoXouUn6OLxH5LkzQnjXCKTPSD7');
